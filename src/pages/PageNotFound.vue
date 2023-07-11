c5adddf022c2f5d8f7b2483cb0c51727d89ca780
Oskar Wiiala <pullupmastery@gmail.com>
2023-07-11 15:44:50 +0300

    Lesson CSS Modules with Webpack done

--- src/pages/PageNotFound.vue
+++ src/pages/PageNotFound.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="col-full">
-    <h1>Page not Found</h1>
-    <p>Oops, we could not find what you are looking for. Why don't you
+  <div class="col-full" :class="$style.centered">
+    <!-- Because the class names included dashes (-), brackets must be used -->
+    <!-- <h1 :class="$style['heading-large']">Page not Found</h1> -->
+
+    <!-- Alternatively, you can use camelcase class names -->
+    <h1 :class="$style.headingLarge">Page not Found</h1>
+    <p class="text-large" :class="$style.textLarge">Oops, we could not find what you are looking for. Why don't you
       <router-link :to="{ name: 'PageHome' }">
         go home instead?
       </router-link>
@@ -15,17 +19,17 @@
   }
 </script>
 
-<style scoped>
-h1 {
+<style module>
+.headingLarge {
   font-size: 100px;
 }
 
-p {
+.textLarge {
   font-size: 50px;
   font-weight: 100;
 }
 
-div {
+.centered {
   text-align: center;
 }
 </style>
